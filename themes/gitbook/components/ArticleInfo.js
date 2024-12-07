05cb5f013e614d8c8273cf749f844e0780f15d99
chris9911-gif <CHRISchris9911@proton.me>
2024-12-07 16:13:45 +0800

    Update ArticleInfo.js

--- themes/gitbook/components/ArticleInfo.js
+++ themes/gitbook/components/ArticleInfo.js
@@ -7,11 +7,5 @@ export default function ArticleInfo({ post }) {
   if (!post) {
     return null
   }
-  return (
-    <div className='pt-10 pb-6 text-gray-400 text-sm'>
-      <i className='fa-regular fa-clock mr-1' />
-      Last update:{' '}
-      {post.date?.start_date || post?.publishDay || post?.lastEditedDay}
-    </div>
-  )
+  return null
 }
